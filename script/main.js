a96c5a4681dee7164402f142fb1bbf9e27f6bc53
DanCan30 <danielecan3008@gmail.com>
2022-06-13 15:44:37 +0200

    Added some memes and minor fixes

--- script/main.js
+++ script/main.js
@@ -5,16 +5,18 @@ const userName = prompt("Insert here your name");
 
 const userSurname = prompt("Insert here your surname");
 
-let userBirthYear = prompt("Insert here your birth year");
-userBirthYear = parseInt(userBirthYear);
+const userBirthYear = prompt("Insert here your birth year");
 
 let userFavouriteColor = prompt("Insert here your favourite color");
 
 let currentYear = new Date().getFullYear();
+currentYear = currentYear.toString();
 
+document.getElementById("ideal-password").innerHTML = userName + userSurname + userBirthYear + userFavouriteColor + currentYear;
 
+console.log(typeof userName, userSurname, userBirthYear, userFavouriteColor, currentYear)
 
-document.getElementById("ideal-password").innerHTML = userName + userSurname + userBirthYear + userFavouriteColor + currentYear
+document.getElementById("team-sunglasses").style.display = "block";
+document.getElementById("team-deal").style.display = "block";
 }
-
 
